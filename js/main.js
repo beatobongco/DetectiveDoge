560a57e73f477dc6ac9bbcb79d9ed487d5bb6c0d
Beato Bongco <beatobongco@gmail.com>
2016-12-07 00:49:53 +0800

    Put functions into vue methods, style loading and menu screens

--- js/main.js
+++ js/main.js
@@ -6,6 +6,7 @@ var app = new Vue({
   el: '.app',
   data: {
     isLoaded: false,
+    isPlaying: false,
     progress: 0,
     level: 1,
     score: 0,
@@ -15,11 +16,151 @@ var app = new Vue({
     lastShot: "",
     powerUpLocation: "",
     prevPPLocation: "",
-    kills: 0
+    kills: 0,
+    hammertime: null
   },
   computed: {
     formattedScore: function() {
       return this.score.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")
+    }
+  },
+  mounted: function() {
+    this.hammertime = new Hammer(document.querySelector(".app"))
+    this.hammertime.on('tap', function(e) {
+      if (app.detectiveState === "dead") {
+        app.resetGame()
+      }
+    })
+  },
+  methods: {
+    startGame: function() {
+      this.isPlaying = true
+      gameLoop = setInterval(this.shootDetective, 1000)
+
+      document.onkeydown = checkKey
+      this.setupTouch()
+      bgm = createjs.Sound.play("bgm", {loop: -1})
+      createjs.Sound.play("footsteps", {loop: 1})
+    },
+    gameOver: function() {
+      var mgs = createjs.Sound.play("machineGun")
+      mgs.volume = 0.25
+      createjs.Sound.play("death")
+      this.powerUpLocation = ""
+      this.hint = ""
+      this.hammertime.off("swipeleft swipedown swipeup swiperight")
+      clearInterval(gameLoop)
+      bgm.stop()
+
+      //play death animation, get visible
+      document.querySelector(".detective." + app.detectiveState).classList.add("bounceOutDown")
+
+      setTimeout(function() {
+        document.querySelector(".detective." + app.detectiveState).classList.remove("bounceOutDown")
+        document.onkeydown = checkReset
+        app.detectiveState = "dead"
+      }, 500)
+    },
+    resetGame: function () {
+      app.level = 1
+      app.score = 0
+      app.detectiveState = "stand"
+      app.hint = ""
+      app.counter = 0
+      app.lastShot = ""
+      app.powerUpLocation = ""
+      app.prevPPLocation = ""
+      app.kills = 0
+      this.startGame()
+    },
+    shootDetective: function() {
+      var shotChoices = ["duck", "left", "right"]
+      //you cannot be shot in the same area as last time
+      //not as powerup
+      var index = shotChoices.indexOf(app.lastShot)
+
+      if (index > -1) {
+        shotChoices.splice(index, 1)
+      }
+
+      if (app.prevPPLocation) {
+        var pIndex = shotChoices.indexOf(app.prevPPLocation)
+
+        if (pIndex > -1) {
+          shotChoices.splice(pIndex, 1)
+        }
+        app.prevPPLocation = ""
+      }
+
+      var aliveChoice = randomChoice(shotChoices)
+
+      app.lastShot = aliveChoice
+      app.hint = aliveChoice
+
+      //chance to spawn powerup
+      if (app.level > 1 && Math.random() < 0.1) {
+        var arrowIndex = shotChoices.indexOf(aliveChoice)
+
+        shotChoices.splice(arrowIndex, 1)
+        app.powerUpLocation = randomChoice(shotChoices)
+        createjs.Sound.play("ahooga")
+      }
+      else {
+        createjs.Sound.play("bark")
+      }
+
+      setTimeout(function() {
+        app.hint = ""
+        if (app.detectiveState === aliveChoice) {
+          var mgs = createjs.Sound.play("machineGun")
+          mgs.volume = 0.25
+          app.surviveRound()
+        }
+        else if (app.detectiveState === app.powerUpLocation) {
+          createjs.Sound.play("pistol")
+          createjs.Sound.play("wilhelm")
+          app.score += app.level * 5000
+          document.querySelector(".mobster:last-child").classList.add("bounceOut")
+          setTimeout(function() {
+            app.level--
+          }, 500)
+          app.kills++
+          app.prevPPLocation = app.powerUpLocation
+          app.surviveRound()
+        }
+        else {
+          app.gameOver()
+        }
+      }, startingSpeed - (app.level * speedDecrement))
+    },
+    surviveRound: function() {
+      this.powerUpLocation = ""
+      this.score += this.level * 1000
+      this.counter++
+
+      if (this.counter % 5 === 0) {
+        createjs.Sound.play("footsteps", {loop: 1})
+        this.level++
+      }
+    },
+    setupTouch: function() {
+      this.hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL })
+      this.hammertime.on('swipeleft', function(e) {
+        app.detectiveState = "left"
+        createjs.Sound.play("whoosh")
+      })
+      this.hammertime.on('swiperight', function(e) {
+        app.detectiveState = "right"
+        createjs.Sound.play("whoosh")
+      })
+      this.hammertime.on('swipeup', function(e) {
+        app.detectiveState = "stand"
+        createjs.Sound.play("whoosh")
+      })
+      this.hammertime.on('swipedown', function(e) {
+        app.detectiveState = "duck"
+        createjs.Sound.play("whoosh")
+      })
     }
   }
 })
@@ -54,39 +195,7 @@ function handleProgress(e) {
 }
 
 function handleComplete() {
-  console.log("Loaded")
   app.isLoaded = true
-  startGame()
-}
-
-
-//handle swipes
-var hammertime = new Hammer(document.querySelector(".app"))
-
-hammertime.on('tap', function(e) {
-  if (app.detectiveState === "dead") {
-    resetGame()
-  }
-})
-
-function setupTouch() {
-  hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL })
-  hammertime.on('swipeleft', function(e) {
-    app.detectiveState = "left"
-    createjs.Sound.play("whoosh")
-  })
-  hammertime.on('swiperight', function(e) {
-    app.detectiveState = "right"
-    createjs.Sound.play("whoosh")
-  })
-  hammertime.on('swipeup', function(e) {
-    app.detectiveState = "stand"
-    createjs.Sound.play("whoosh")
-  })
-  hammertime.on('swipedown', function(e) {
-    app.detectiveState = "duck"
-    createjs.Sound.play("whoosh")
-  })
 }
 
 function checkKey(e) {
@@ -109,127 +218,13 @@ function checkKey(e) {
   }
 }
 
-function resetGame() {
-  app.level = 1
-  app.score = 0
-  app.detectiveState = "stand"
-  app.hint = ""
-  app.counter = 0
-  app.lastShot = ""
-  app.powerUpLocation = ""
-  app.prevPPLocation = ""
-  app.kills = 0
-  startGame()
-}
-
 function checkReset(e) {
   e = e || window.event
   if (e.keyCode == '82') {
-    resetGame()
+    app.resetGame()
   }
 }
 
 function randomChoice(arr) {
   return arr[Math.floor(arr.length * Math.random())];
-}
-
-function surviveRound() {
-  app.powerUpLocation = ""
-  app.score += app.level * 1000
-  app.counter++
-
-  if (app.counter % 5 === 0) {
-    createjs.Sound.play("footsteps", {loop: 1})
-    app.level++
-  }
-}
-
-function gameOver() {
-  var mgs = createjs.Sound.play("machineGun")
-  mgs.volume = 0.25
-  createjs.Sound.play("death")
-  app.powerUpLocation = ""
-  app.hint = ""
-  hammertime.off("swipeleft swipedown swipeup swiperight")
-  clearInterval(gameLoop)
-  document.onkeydown = checkReset
-  bgm.stop()
-
-  //play death animation, get visible
-  document.querySelector(".detective." + app.detectiveState).classList.add("bounceOutDown")
-
-  setTimeout(function() {
-    document.querySelector(".detective." + app.detectiveState).classList.remove("bounceOutDown")
-    app.detectiveState = "dead"
-  }, 500)
-}
-
-function shootDetective() {
-  var shotChoices = ["duck", "left", "right"]
-  //you cannot be shot in the same area as last time
-  //not as powerup
-  var index = shotChoices.indexOf(app.lastShot)
-
-  if (index > -1) {
-    shotChoices.splice(index, 1)
-  }
-
-  if (app.prevPPLocation) {
-    var pIndex = shotChoices.indexOf(app.prevPPLocation)
-
-    if (pIndex > -1) {
-      shotChoices.splice(pIndex, 1)
-    }
-    app.prevPPLocation = ""
-  }
-
-  var aliveChoice = randomChoice(shotChoices)
-
-  app.lastShot = aliveChoice
-  app.hint = aliveChoice
-
-  //chance to spawn powerup
-  if (app.level > 1 && Math.random() < 1) {
-    var arrowIndex = shotChoices.indexOf(aliveChoice)
-
-    shotChoices.splice(arrowIndex, 1)
-    app.powerUpLocation = randomChoice(shotChoices)
-    createjs.Sound.play("ahooga")
-  }
-  else {
-    createjs.Sound.play("bark")
-  }
-
-  setTimeout(function() {
-    app.hint = ""
-    if (app.detectiveState === aliveChoice) {
-      var mgs = createjs.Sound.play("machineGun")
-      mgs.volume = 0.25
-      surviveRound()
-    }
-    else if (app.detectiveState === app.powerUpLocation) {
-      createjs.Sound.play("pistol")
-      createjs.Sound.play("wilhelm")
-      app.score += app.level * 5000
-      document.querySelector(".mobster:last-child").classList.add("bounceOut")
-      setTimeout(function() {
-        app.level--
-      }, 500)
-      app.kills++
-      app.prevPPLocation = app.powerUpLocation
-      surviveRound()
-    }
-    else {
-      gameOver()
-    }
-  }, startingSpeed - (app.level * speedDecrement))
-}
-
-function startGame() {
-  gameLoop = setInterval(shootDetective, 1000)
-
-  document.onkeydown = checkKey
-  setupTouch()
-  bgm = createjs.Sound.play("bgm", {loop: -1})
-  createjs.Sound.play("footsteps", {loop: 1})
 }
